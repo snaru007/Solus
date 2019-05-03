d8e3a2ab5dbceb590a3bc37efda395e3084c9919
snaru <shibu.naru@gmail.com>
2019-05-02 20:59:26 -0400

    Tested game window, library is fully integrated

--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,2 +1,11 @@
+import de.gurkenlabs.litiengine.Game;
+
 public class Engine {
+
+    public static void main(String[] args) {
+        Game.init(args);
+        Game.start();
+
+        System.out.println("Game version is: " + Game.info().getVersion());
+    }
 }
